679a3ced7f78ae3165ad95666fa3e41ec8499eda
Anton Shchukin <a.a.shchukin@gmail.com>
2023-05-24 17:32:36 +0700

    compile

--- production/scripts/_flyover.js
+++ production/scripts/_flyover.js
@@ -1,53 +1,77 @@
+/**
+ * Здесь сложный момент с тем что на мобилках и десктопах разное поведение.
+ * На десктопах показ флайоверов случается по mouseenter и mouseleave и нужно считать координаты.
+ * На смартфонах показ по обычному click.
+ * Сложность в том, что бы понять что есть десптоп, а что смартфорн.
+ * Будем следовать за адаптивностью в стилях (ширина 992+) и проверять условие:
+ * window.matchMedia('(min-width: 992px)').matches
+ */
+
 (function($) {
-    var target;
-    var peak;
 
-    $('[data-flyover]').on('mouseenter', function () {
+    if (window.matchMedia('(min-width: 992px)').matches) {
 
-        target = $($(this).attr('data-flyover'));
+        $('[data-flyover]').on('mouseenter', function () {
 
-        if( target.hasClass('flyover--avatar') ) {
-            target.css({
-                'bottom': $(window).outerHeight() + 8 - $(this)[0].getBoundingClientRect().top,
-                'left': $(this)[0].getBoundingClientRect().left - (target.outerWidth() / 2) + ($(this).outerWidth() / 2)
-            });
-        }
+            var target = $($(this).attr('data-flyover'));
 
-        // Few more examples:
-        // if( target.hasClass('flyover--side-card') ) {
-        //     target.css({
-        //         'top': $(this)[0].getBoundingClientRect().top + $(this).height(),
-        //         'right': $(window).outerWidth() - $(this)[0].getBoundingClientRect().left - $(this).outerWidth(),
-        //         'padding-top': '15px'
-        //     });
-        // }
-        //
-        // if (target.hasClass('flyover--org-angle')) {
-        //     target.css({
-        //         'top': $(this)[0].getBoundingClientRect().top + 36,
-        //         'right': $(window).outerWidth() - $(this)[0].getBoundingClientRect().left - 26,
-        //         'padding-bottom': '15px'
-        //     });
-        // }
+            if (target.hasClass('flyover--avatar')) {
+                target.css({
+                    'bottom': $(window).outerHeight() - $(this)[0].getBoundingClientRect().top,
+                    'left': $(this)[0].getBoundingClientRect().left - (target.outerWidth() / 2) + ($(this).outerWidth() / 2)
+                });
+            }
 
-        target.addClass('flyover--visible');
+            // Ещё несколько примеров:
+            // if( target.hasClass('flyover--side-card') ) {
+            //     target.css({
+            //         'top': $(this)[0].getBoundingClientRect().top + $(this).height(),
+            //         'right': $(window).outerWidth() - $(this)[0].getBoundingClientRect().left - $(this).outerWidth(),
+            //         'padding-top': '15px'
+            //     });
+            // }
+            //
+            // if (target.hasClass('flyover--org-angle')) {
+            //     target.css({
+            //         'top': $(this)[0].getBoundingClientRect().top + 36,
+            //         'right': $(window).outerWidth() - $(this)[0].getBoundingClientRect().left - 26,
+            //         'padding-bottom': '15px'
+            //     });
+            // }
 
-    }).on('mouseleave', function () {
-        target.removeClass('flyover--visible');
-    });
+            target.addClass('flyover--visible');
 
+        }).on('mouseleave', function () {
+            $('.flyover').removeClass('flyover--visible');
+        });
 
-    /* to reload on next hover */
-    $(window).on('scroll', function () {
-        $('.flyover').removeClass('flyover--visible');
-    });
+        $(window).on('scroll', function () {
+            $('.flyover').removeClass('flyover--visible');
+        });
 
-    $('.scrollbar').on('scroll', function () {
-        $('.flyover').removeClass('flyover--visible');
-    });
+        $('.scrollbar').on('scroll', function () {
+            $('.flyover').removeClass('flyover--visible');
+        });
 
-    $('.carousel__container').on('scroll', function () {
-        $('.flyover').removeClass('flyover--visible');
-    });
+        $('.carousel__container').on('scroll', function () {
+            $('.flyover').removeClass('flyover--visible');
+        });
+
+    } else {
+
+        $('[data-flyover]').on('click', function () {
+            $($(this).attr('data-flyover')).addClass('flyover--visible');
+        });
+
+        $('[data-close-flyover]').on('click', function () {
+            $(this).parents('.flyover').removeClass('flyover--visible');
+        });
+
+        $('.flyover').on('click', function (event) {
+            if (!$(event.target).closest('.window').length) {
+                $(this).removeClass('flyover--visible');
+            }
+        });
+    }
 
 })(jQuery);
